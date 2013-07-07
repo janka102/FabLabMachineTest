bffca036c4624e3cb228dec2fb50aeeb7e8d9603
Jesse Smick <janka102@gmail.com>
2013-07-07 23:23:18 +0000

    Added a "draggable" and "droppable" directive makeing use of jQueryUI and its draggable and droppable feature for the machine specific pages

--- js/app.js
+++ js/app.js
@@ -19,4 +19,53 @@ var app = angular.module('fabLab', [], function($routeProvider) {
         $routeProvider.otherwise({
             redirectTo: '/'
         });
+}).directive('draggable', function() {
+    return {
+        restrict: 'A',
+        link: function(scope, elm, attrs) {
+            var vars = scope.$eval(attrs.draggable),
+                options = {
+                    addClass: false,
+                    cursorAt: { left: 17, top: 17 },
+                    revert: true,
+                    zIndex: 900,
+                    scope: vars.scope,
+                    helper: function(event) {
+                        return $('<div data-part-id="' + vars.helper.view + vars.helper.letter + '">'+ vars.helper.letter + '</div>');
+                    },
+                    start: function(event, ui) {
+                        var self = $(this);
+                        if (self.hasClass('ui-state-disabled')) {
+                            self.draggable({ disabled: true });
+                        } else {
+                            self.addClass('ui-state-disabled');   
+                        }
+                    },
+                    stop: function(evenr, ui) {
+                        var self = $(this);
+                        if (self.hasClass('ui-state-disabled')) {
+                            self.draggable({ disabled: false });
+                            self.removeClass('ui-state-disabled');
+                        }
+                    }
+                };
+            
+            elm.draggable(options);
+            console.log('draggable:', options);
+        }
+    };
+}).directive('droppable', function() {
+    return {
+        restrict: 'A',
+        link: function(scope, elm, attrs) {
+            var vars = scope.$eval(attrs.droppable),
+                options = {
+                    hoverClass: 'ui-state-hover',
+                    scope: vars.scope
+                };
+                
+            elm.droppable(options);
+            console.log('droppable:', options);
+        }
+    };
 });
