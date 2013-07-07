50c36c0b8af8a73e01d4c8bc6d06d9a92d1af2ef
Jesse Smick <janka102@gmail.com>
2013-07-07 23:25:27 +0000

    Added new bandsaw parts. Added letters to display to the left of each part. Added functions to toggle the descriptions and get location of each part

--- js/controllers.js
+++ js/controllers.js
@@ -11,12 +11,26 @@ var machines = [
             front: [
                 {name: 'On/Off Switch',
                 description: 'Makes the machine run or stop, but does not cut power to the machine and should never be substituted for unplugging the power when required.',
-                x: 200, y: 150}  //Temporary
+                count: 1,
+                x: 87, y: 340},  //Temporary locations
+                {name: 'Fence Lock Handle',
+                description: 'Locks the fence in place for cutting operations and unlocks the fence for adjustments or removal.',
+                count: 1,
+                x: 80, y: 460},  //Temporary locations
+                {name: 'Hinged Wheel Covers',
+                description: 'Allows wheels and pulleys to be easily accessed for maintenance, blade changes, or adjestmens.',
+                count: 2,
+                x: 190, y: 130}  //Temporary locations
             ],
             rear: [
                 {name: 'Blade Tension Adjustment Knob',
                 description: 'Controls minor/moderate blade tension adjustments.',
-                x: 100, y: 50} //Temporary
+                count: 1,
+                x: 100, y: 50}, //Temporary locations
+                {name: 'Blade Tracking Adjustment Knob',
+                description: 'Controls the tilt angle of the upper wheel, which defines how the blade will track on the wheel.',
+                count: 1,
+                x: 150, y: 100} //Temporary locations
             ]
         }},
 
@@ -69,7 +83,8 @@ var machines = [
             front: [
             ]
         }}
-    ];
+    ],
+    alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 
 function MainCtrl($scope) {
     $scope.machines = machines;
@@ -97,7 +112,34 @@ function NavCtrl($scope, $location) {
     };
 }
 
-function MachineCtrl($scope) {
-    $scope.machines = machines;
+function MachineCtrl($scope, $location, $routeParams) {
+    var paramMachine = $routeParams.machine,
+        validMachine = false;
+    
+    for (var i = 0, len = machines.length; i < len; i++) {
+        if (machines[i].id === paramMachine) {
+            validMachine = true;
+            $scope.machine = machines[i];
+            break;
+        }
+    }
+    
+    if (!validMachine) {
+        $location.path('/');
+        return;
+    }
+    
+    // For displaying the parts
+    $scope.alphabet = alphabet;
+    
+    $scope.toggle = function(part){
+        $('.' + part).toggleClass('ui-helper-hidden-accessible');
+        
+        console.log('toggled', part);
+    };
+    
+    $scope.styleLocation = function(part) {
+        return {top: part.y, left: part.x};  
+    };
 }
 
