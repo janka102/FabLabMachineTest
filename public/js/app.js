3af0c2208df755c980790deb6edbc220f0a87646
Jesse Smick <janka102@gmail.com>
2013-08-02 01:41:08 -0700

    Got rid of unused code and fixed problem where the total score would be calculated even if not all parts were placed

--- public/js/app.js
+++ public/js/app.js
@@ -30,7 +30,7 @@ function checkSubmit(){
 
         numCorrect = 0,
         partsLength = 0,
-        unfilledViews = 0,
+        incompleteViews = 0,
         score = function(percent) {
             var bad = [':O', ':(', '(O.O)', 'ಠ╭╮ಠ', 'ರ_ರ', 'you should probably try again'],
                 medium = ['maybe next time you\'ll get it'],
@@ -57,12 +57,8 @@ function checkSubmit(){
                 scoreMessage = good[Math.floor(Math.random() * good.length)];
             }
 
-            return {"klass": cssClass, "message": scoreMessage};
-        };
-        
-    if ($('.alert-error')) {
-        $('.alert-error').remove();
-    }
+            return {'klass': cssClass, 'message': scoreMessage};
+        }
 
     machineViews.each(function(index){
         var self = $(this),
@@ -76,10 +72,11 @@ function checkSubmit(){
             scoreObj;
 
         if (filledviewPartsLength === 0) {
-            unfilledViews += 1;
+            incompleteViews += 1;
             self.find('.score').html(message1.replace('%msg%', 'You did not fill in any parts!'));
             return;
         } else if (filledviewPartsLength < viewPartsLength) {
+            incompleteViews += 1;
             self.find('.score').html(message1.replace('%msg%', 'You forgot ' + (viewPartsLength - filledviewPartsLength) + ' part' + ((viewPartsLength - filledviewPartsLength) !== 1 ? 's' : '') + '!'));
             return;
         }
@@ -105,66 +102,19 @@ function checkSubmit(){
             self.find('.score').html(message2.replace(/text-[a-z]+/, scoreObj.klass).replace('%msg%', 'You got ' + viewNumCorrect + ' out of ' + viewPartsLength + ' correct, that\'s ' + percent.toFixed(2) + '%... ' + scoreObj.message));
         }
     
-        if (index === machineViews.length - 1 && unfilledViews === 0) {
+        if (index === machineViews.length - 1 && incompleteViews === 0) {
             scoreObj = score(numCorrect / partsLength * 100);
             $('#check-parts').find('.score').html(message2.replace(/text-[a-z]+/, scoreObj.klass).replace('%msg%', '<strong>Total:</strong> You got ' + numCorrect + ' out of ' + partsLength + ' correct, that\'s ' + ((numCorrect / partsLength) * 100).toFixed(2) + '%... ' + scoreObj.message));
         }
     });
-
-/*
-    if (filledviewPartsLength < viewPartsLength) {
-        //Fill in the number of unfilled parts and add an 's' if the number is not 1
-        partAlert = $(alert.replace('#', ).replace('$', (viewPartsLength - filledviewPartsLength) !== 1 ? 's' : ''));
-        
-        $('.details').prepend(partAlert);
-        return;
-    }
-        
-    for (var i = 0, currPart; i < viewPartsLength; i++) {
-        currPart = parts.eq(i);
-        
-        if (currPart.data('part-id') === currPart.children().eq(0).data('part-id')) {
-            viewNumCorrect++;
-        }
-    }
-    
-    percent = (viewNumCorrect / viewPartsLength) * 100;
-    
-    switch(true) {
-    // [0, 70] is red
-    case (percent >= 0 && percent <= 70):
-        cssClass = 'text-error';
-        message = bad[Math.floor(Math.random() * bad.length)];
-        break;
-        
-    // (70, 90) is yellow
-    case (percent > 70 && percent < 90):
-        cssClass = 'text-warning';
-        message = medium[Math.floor(Math.random() * medium.length)];
-        break;
-        
-    // [90, 100] is green
-    default:
-        cssClass = 'text-success';
-        message = good[Math.floor(Math.random() * good.length)];
-    }
-    
-    //console.log('correct:', viewNumCorrect, 'total:', viewPartsLength);
-
-    $('#check-parts').find('.score').removeClass().addClass('score ' + cssClass).html('You got ' + viewNumCorrect + ' out of ' + viewPartsLength + ' correct, that\'s ' + percent.toFixed(2) + '%... ' + message);
-*/
-
 }
 
 function resetParts() {
     var partsList = $('.parts-list'),
         partDrops = $('.part-drop');
         
-    if ($('.alert-error')) {
-        $('.alert-error').remove();
-    }
-    
-    $('#check-parts').find('.score').html('').removeClass().addClass('score');
+    // Clear out any scores/alerts
+    $('.score').html('');
     
     partsList.children().each(function(){
         $(this).data('dropped', false).draggable('enable').removeClass('ui-state-disabled');
